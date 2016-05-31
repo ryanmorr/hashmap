9b61c1c8beea6553ea8972eed72de6abbaec7956
Ryan Morr <rm.morr@gmail.com>
2016-05-31 13:43:30 -0400

    Add basic functionality

---
+++ src/hashmap.js
@@ -0,0 +1,9 @@
+/**
+ * Create a basic hashmap
+ *
+ * @return {Object}
+ * @api public
+ */
+export default function hashmap() {
+    return Object.create(null);
+}
---
+++ test/test-hasmap.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import hashmap from '../src/hashmap';
+
+describe('hashmap', () => {
+    it('should be an object', () => {
+        const map = hashmap();
+        expect(map).to.be.an('object');
+    });
+
+    it('should support assigning and reading properties', () => {
+        const map = hashmap();
+        map.foo = 1;
+        map.bar = 2;
+
+        expect(map['foo']).to.equal(1); // eslint-disable-line dot-notation
+        expect(map.bar).to.equal(2);
+    });
+
+    it('should support enumeration', () => {
+        const map = hashmap();
+        map.foo = 1;
+        map.bar = 2;
+
+        let i = 0;
+        const keys = Object.keys(map);
+        for (const prop in map) {
+            expect(prop).to.equal(keys[i]);
+            expect(map[prop]).to.equal(map[keys[i]]);
+            i++;
+        }
+    });
+
+    it('should not have a prototype or inherited properties/methods', () => {
+        const map = hashmap();
+        const props = [
+            'isPrototypeOf',
+            'hasOwnProperty',
+            'toLocaleString',
+            'propertyIsEnumerable',
+            'toString',
+            'valueOf',
+            'constructor'
+        ];
+
+        expect(Object.getPrototypeOf(map)).to.equal(null);
+        expect(props.every(key => key in map)).to.equal(false);
+    });
+});
