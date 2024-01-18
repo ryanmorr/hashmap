eddfbe588c02a629b13b852aede6d881cf2f8b5c
Ryan Morr <rm.morr@gmail.com>
2024-01-18 12:30:09 -0500

    Build v0.1.3

---
+++ dist/cjs/hashmap.js
@@ -0,0 +1,2 @@
+/*! @ryanmorr/hashmap v0.1.3 | https://github.com/ryanmorr/hashmap */
+"use strict";module.exports=function(...e){const t=Object.create(null,{[Symbol.iterator]:{enumerable:!1,writable:!1,configurable:!1,value:()=>({items:Object.entries(t),next:function(){return{done:0===this.items.length,value:this.items.shift()}}})}});return e.length&&Object.assign(t,...e),t};
---
+++ dist/umd/hashmap.js
@@ -0,0 +1,2 @@
+/*! @ryanmorr/hashmap v0.1.3 | https://github.com/ryanmorr/hashmap */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e="undefined"!=typeof globalThis?globalThis:e||self).hashmap=t()}(this,(function(){"use strict";return function(...e){const t=Object.create(null,{[Symbol.iterator]:{enumerable:!1,writable:!1,configurable:!1,value:()=>({items:Object.entries(t),next:function(){return{done:0===this.items.length,value:this.items.shift()}}})}});return e.length&&Object.assign(t,...e),t}}));
